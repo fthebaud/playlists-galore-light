01b84a3d257f9d6dfcc3756c3ab299a2ddeb851c
Fabien Thebaud <fthebaud@gmail.com>
2022-10-16 17:12:06 +0200

    Temporary fix for caching issue

--- src/modules/client/client.ts
+++ src/modules/client/client.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { PlaylistsResponse } from '@/types';
 
+// TODO hash in playlists.json filename or find a way to prevent caching
 // fetch all playlists from public/data/playlists.json
 export async function fetchAllPlaylists(): Promise<PlaylistsResponse> {
   const { data } = await axios.get<PlaylistsResponse>(
-    `${window.location.origin}/data/playlists.json`
+    `${window.location.origin}/data/playlists-202210161705.json`
   );
   return data;
 }
