7a7e13ca73100642e30f9b0b54b2d92c96c1dcce
Fabien Thebaud <fthebaud@gmail.com>
2022-12-28 19:25:55 +0100

    Playlist: December 2022

--- src/modules/client/client.ts
+++ src/modules/client/client.ts
@@ -5,7 +5,7 @@ import { PlaylistsResponse } from '@/types';
 // fetch all playlists from public/data/playlists.json
 export async function fetchAllPlaylists(): Promise<PlaylistsResponse> {
   const { data } = await axios.get<PlaylistsResponse>(
-    `${window.location.origin}/data/playlists-202212111355.json`
+    `${window.location.origin}/data/playlists-202212261851.json`
   );
   return data;
 }
