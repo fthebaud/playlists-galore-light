0c32d54183241265e8d70a5d6a78c9d4de6600e9
Fabien Thebaud <fthebaud@gmail.com>
2023-08-27 18:49:18 +0200

    Playlist: August 2023

--- src/modules/client/client.ts
+++ src/modules/client/client.ts
@@ -5,7 +5,7 @@ import { PlaylistsResponse } from '@/types';
 // fetch all playlists from public/data/playlists.json
 export async function fetchAllPlaylists(): Promise<PlaylistsResponse> {
   const { data } = await axios.get<PlaylistsResponse>(
-    `${window.location.origin}/data/playlists-202306161328.json`
+    `${window.location.origin}/data/playlists-202308271841.json`
   );
   return data;
 }
